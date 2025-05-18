87a69b8c5e171cabf8b11977913b4f7630a1a648
DevPatata <anavaro.sala@gmail.com>
2025-05-18 21:28:57 +0200

    execute_exit less than 25 lines

--- src/builtins/Exit.c
+++ src/builtins/Exit.c
@@ -6,7 +6,7 @@
 /*   By: angnavar <angnavar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 22:12:48 by angnavar          #+#    #+#             */
-/*   Updated: 2025/05/18 21:16:11 by angnavar         ###   ########.fr       */
+/*   Updated: 2025/05/18 21:27:49 by angnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ int	check_exit_cmd(char *input)
 	return (0);
 }
 
-void exit_free(t_shell *mn_shell, int exit_status)
+void exit_free(t_shell *mn_shell, int exit_status, int printerror)
 {
+	if(printerror)
+		perr_name(mn_shell, "exit", "numeric argument required", 2);
 	free_all(NULL, mn_shell);
 	free_env(mn_shell->envp);
 	free(mn_shell);
@@ -30,8 +32,9 @@ void exit_free(t_shell *mn_shell, int exit_status)
 int	execute_exit(t_cmd *cmds, t_shell *mn_shell)
 {
 	int		exit_status;
-	int		i = 0;
+	int		i;
 
+	i = 0;
 	if (cmds->next || !check_exit_cmd(cmds->args[0]))
 		return (0);
 	ft_putendl_fd("exit", 1);
@@ -40,10 +43,7 @@ int	execute_exit(t_cmd *cmds, t_shell *mn_shell)
 		while (cmds->args[1][i])
 		{
 			if (!ft_isdigit(cmds->args[1][i]))
-			{
-				perr_name(mn_shell, "exit", "numeric argument required", 2);
-				exit_free( mn_shell, 2);
-			}
+				exit_free( mn_shell, 2, 1);
 			i++;
 		}
 		if (cmds->args[2])
@@ -52,7 +52,7 @@ int	execute_exit(t_cmd *cmds, t_shell *mn_shell)
 	}
 	else
 		exit_status = mn_shell->last_exit_code;
-	exit_free( mn_shell, exit_status);
-	return (exit_status);
+	exit_free( mn_shell, exit_status, 0);
+	exit (exit_status);
 }
 
