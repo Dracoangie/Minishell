12148cdf7c53042e8ae15c65c40e6a204124b7ed
DevPatata <anavaro.sala@gmail.com>
2025-05-21 02:00:58 +0200

    cmd in between redirs

--- src/Parse/Parse_redirs.c
+++ src/Parse/Parse_redirs.c
@@ -6,14 +6,46 @@
 /*   By: angnavar <angnavar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 22:21:31 by angnavar          #+#    #+#             */
-/*   Updated: 2025/05/21 01:23:54 by angnavar         ###   ########.fr       */
+/*   Updated: 2025/05/21 01:59:10 by angnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Minishell.h"
 
-void	parse_redirs(t_shell *mn, t_cmd *cmd)
+int	parse_redirs(t_shell *mn, t_cmd *cmd)
 {
+	int		i;
+	int		j;
+	char	*first_cmd;
+
 	(void)mn;
-	(void)cmd;
+	i = 0;
+	while (cmd->args[i])
+	{
+		if ((ft_strcmp(cmd->args[i], "<") == 0 || ft_strcmp(cmd->args[i],
+					">") == 0 || ft_strcmp(cmd->args[i], ">>") == 0
+				|| ft_strcmp(cmd->args[i], "<<") == 0) && cmd->args[i + 1])
+		{
+			i += 2;
+		}
+		else if (cmd->args[i][0] != '<' && cmd->args[i][0] != '>')
+		{
+			break ;
+		}
+		else
+			i++;
+	}
+	if (!cmd->args[i])
+		return (0);
+	if (i == 0)
+		return (1);
+	first_cmd = cmd->args[i];
+	j = i;
+	while (j > 0)
+	{
+		cmd->args[j] = cmd->args[j - 1];
+		j--;
+	}
+	cmd->args[0] = first_cmd;
+	return (1);
 }
